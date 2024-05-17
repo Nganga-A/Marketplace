c87ffa6cbb81c66a7d99170707c0a1e3b26e35fb
Abed <ngangaabed@gmail.com>
2024-05-17 23:12:09 +0300

    Setting up files

---
+++ marketplace/App.js
@@ -0,0 +1,11 @@
+import { StatusBar, StyleSheet,Text,View } from "react-native"
+
+export default function App() {
+    return (
+        <View>
+            <Text>Lets Go</Text>
+            <StatusBar style="auto"/>
+        </View>
+    );
+}
+
