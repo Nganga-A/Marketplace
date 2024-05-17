d67a0552562f4b40319140a2b733860c6ba55f6a
Abed <ngangaabed@gmail.com>
2024-05-18 01:34:09 +0300

    Added login screen and image

---
+++ marketplace/app/Screens/LoginScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function LoginScreen() {
+    return (
+        <View>
+            <Image source={'./../../assets/images/login.jpg'}></Image>
+        </View>
+    )
+}
--- marketplace/app/index.js
+++ marketplace/app/index.js
@@ -1,11 +1,12 @@
 import { StyleSheet,Text,View } from "react-native"
 import { StatusBar } from "expo-status-bar";
+import LoginScreen from "./Screens/LoginScreen";
 
 export default function App() {
     return (
-        <View className="flex items-center justify-center bg-white">
-            <Text className="text-[30px]">Lets Go Abed</Text>
+        <View className="flex-1 items-center bg-white"> 
             <StatusBar style="auto"/>
+            <LoginScreen/>
         </View>
     );
 }
