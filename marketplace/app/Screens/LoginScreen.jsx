ad0cb2265a25f65c65dc9b7a49c2f9767712c763
Abed <ngangaabed@gmail.com>
2024-05-20 01:07:03 +0300

    Login header styling

--- marketplace/app/Screens/LoginScreen.jsx
+++ marketplace/app/Screens/LoginScreen.jsx
@@ -1,18 +1,39 @@
-import { View, Text, Image,StyleSheet } from 'react-native'
 import React from 'react'
+import { View, Text, Image,TouchableOpacity,StyleSheet } from 'react-native'
+
 
 export default function LoginScreen() {
     return (
-        <View>
+        <View style={styles.container}>
             <Image source={require('./../../assets/images/login.jpg')}
-            className="w-full h-[400px] object-cover"/>
-            <View className="p-5">
-                <Text className="text-[25px] font-bold">Community Marketplace</Text>
+            style={styles.image}
+            onLoad={() => console.log('Image loaded')}
+            onError={(e) => console.log('Error loading image', e.nativeEvent.error)}
+            />
+            <View className="p-5 bg-white mt-[-20px] rounded-t-3xl shadow-md">
+                <Text className="text-[25px] text-center font-bold">Community Marketplace</Text>
                 <Text className="text-[18px] text-slate-500 mt-4">Buy Sell Marketplace where you can sell old items and make real money</Text>
-            </View>
-            <View className="p-4 bg-blue-500 rounded-full">
-                <Text className="text-white text-center">Get Started </Text>
+                <TouchableOpacity className="p-4 mt-4 bg-blue-500 rounded-full">
+                <Text className="text-white text-center text-[18px]">Get Started </Text>
+                </TouchableOpacity>
+                
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 400,
+        resizeMode: 'cover',
+        position:'relative',
+        zIndex:10,
+    },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: '#f7f3',
+    }
+});
