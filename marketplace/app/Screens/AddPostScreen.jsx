cfa543b8ad19ef1fb785e5d7c43f6d287215a7ac
Abed <ngangaabed@gmail.com>
2024-06-12 00:07:51 +0300

    Firebase setup

--- marketplace/app/Screens/AddPostScreen.jsx
+++ marketplace/app/Screens/AddPostScreen.jsx
@@ -1,7 +1,24 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import {app} from '../../firebaseConfig'
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+
 
 export default function AddPostScreen() {
+
+    const db = getFirestore(app);
+    
+    useEffect(()=> {
+        getCategoryList();
+    },[]);
+
+    const getCategoryList = async() => {
+        const querySnapshot = await getDocs(collection(db, "category"));
+        querySnapshot.forEach((doc) => {
+            console.log("Docs:",doc.data());
+        })
+    }
+
     return (
         <View>
             <Text>AddPostScreen</Text>
